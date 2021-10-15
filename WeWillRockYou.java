e420c33f5457d29dd3379db9d827a67854757605
Zhengfei Ji <zhengfj@amazon.com>
2021-10-14 20:27:36 -0700

    commit msg

--- WeWillRockYou.java
+++ WeWillRockYou.java
@@ -6,4 +6,12 @@ public class WeWillRockYou {
     private static void iLike1() {
         System.out.println("Another one bites the dust");
     }
+	private static void queenXD0a3f9759-f7c2-4cab-864c-930b35c8e22c() {
+		System.out.println(BohemianRhapsody);
+	}
+
+	private static void queenXD6f011de1-4388-43a1-95f7-9fa47be37b9c() {
+		System.out.println(AnotherOneBitesTheDust);
+	}
+
 }
